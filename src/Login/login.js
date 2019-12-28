3a0b6cbea2daefcbeb6310bcec39ac79e9c55d02
2470315829 <2470315829@qq.com>
2019-12-28 23:52:35 +0800

    登陆界面

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 var LoginCss = require("./login.css")
+
 export default class LoginPage extends React.Component{
 	render(){
 		return(
 			<div className={LoginCss.login}>
-				<h1>登陆界面</h1>
-                <p>用户名：</p>
-                <p>密码：</p>
+				<div className={LoginCss.back}>
+				
+				<p className={LoginCss.title}>希望二手交易平台</p>
+
+				<p className={LoginCss.username}>用户名：</p>
+				<div className={LoginCss.userinput}>
+				<input  className={LoginCss.input}></input>
+				</div>
+
+                <p className={LoginCss.password}>密码：</p>
+				<div className={LoginCss.passinput}>
+				<input className={LoginCss.input}></input>
+				</div>
+				<div className={LoginCss.buttonbox}>
+				<button className={LoginCss.button} placeholder="登录" ></button>
+				</div>
+				</div>
+                
 			</div>
 		)
 	}
