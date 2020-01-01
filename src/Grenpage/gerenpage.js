cc3c366d86eb958917d86fd6b7380bbe5e3aac6f
2470315829 <2470315829@qq.com>
2020-01-01 22:20:56 +0800

    ajax

--- src/Grenpage/gerenpage.js
+++ src/Grenpage/gerenpage.js
@@ -5,7 +5,56 @@ const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout
 var gerenpageCss = require("./gerenpage.css")
 export default class GERENpage extends React.Component{
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 	  error: null,
+	// 	  isLoaded: false,
+	// 	  items: []
+	// 	};
+	//   }
+	
+	//   componentDidMount() {
+	// 	fetch("")
+	// 	  .then(res => res.json())
+	// 	  .then(
+	// 		(result) => {
+	// 		  this.setState({
+	// 			isLoaded: true,
+	// 			items: result.items
+	// 		  });
+	// 		},
+	// 		// 注意：需要在此处处理错误
+	// 		// 而不是使用 catch() 去捕获错误
+	// 		// 因为使用 catch 去捕获异常会掩盖掉组件本身可能产生的 bug
+	// 		(error) => {
+	// 		  this.setState({
+	// 			isLoaded: true,
+	// 			error
+	// 		  });
+	// 		}
+	// 	  )
+	//   }
+
+
 	render(){
+	// 	const { error, isLoaded, items } = this.state;
+    // if (error) {
+    //   return <div>Error: {error.message}</div>;
+    // } else if (!isLoaded) {
+    //   return <div>Loading...</div>;
+    // } else {
+    //   return (
+    //     <ul>
+    //       {items.map(item => (
+    //         <li key={item.name}>
+    //           {item.name} {item.price}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   );
+    // }
+  
 		return(
 			
 
